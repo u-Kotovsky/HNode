cee71df325d9be21cf97329227cfe71e29f57f2e
Matthew Herber <mherber@bluewatercontrols.com>
2025-06-30 10:13:19 -0400

    Get spout input working from OBS

---
+++ Assets/TextureReader.cs
@@ -0,0 +1,18 @@
+using Klak.Spout;
+using UnityEngine;
+
+public class TextureReader : MonoBehaviour
+{
+    public Loader loader;
+    public SpoutReceiver spoutReceiver;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
