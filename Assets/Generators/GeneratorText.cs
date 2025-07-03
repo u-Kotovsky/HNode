e6cf4ce7da307ad04829d41d0517a30047a1e270
Matthew Herber <matthewherber@sbcglobal.net>
2025-07-03 17:18:40 -0400

    Add cleaner dictionary

--- Assets/Generators/GeneratorText.cs
+++ Assets/Generators/GeneratorText.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static Unity.Collections.Unicode;
 
 public class Text : IDMXGenerator
 {
@@ -25,8 +26,78 @@ public class Text : IDMXGenerator
 
     public virtual void Construct() { }
 
+    //I swear to god to anyone reading this code. This is like, the 5TH FUCKING TIME ive had to write a system like this
+    //its unbelievable that there isnt a standard for fallback character conversion considering the ammount of times
+    //its been a issue in my codebases. Seriously WTF
+    //This one is based from scratcher bot
+    public static readonly Dictionary<char, string> CleanupTable = new() //WARNING! Make sure there is no duplicate keys or it will cause a runtime error
+    {
+        { '‘', "\'" },
+        { '’', "\'" },
+        { '“', "\"" },
+        { '”', "''" },
+        { '„', ",," },
+        { '‟', "\"" },
+        { '‹', "<" },
+        { '›', ">" },
+        { '‚', "," },
+        { '‛', "'" },
+        { '¡', "!" },
+        { '¿', "?" },
+        { '‼', "!!" },
+        { '⁇', "??" },
+        { '⁈', "?!" },
+        { '⁉', "!?" },
+        { '‽', "?!" },
+        { '†', "+" },
+        { '‡', "++" },
+        { '․', "." },
+        { '‥', ".." },
+        { '…', "..." },
+        { '‰', "%" },
+        { '‱', "%%" },
+        { '′', "'" },
+        { '″', "\"" },
+        { '‴', "\"" },
+        { '‵', "`" },
+        { '‶', "\"" },
+        { '‷', "\"" },
+        { '‸', "^" },
+        { '※', "*" },
+        { '⁂', "***" },
+        { '⁄', "/" },
+        { '⁎', "*" },
+        { '⁏', ";" },
+        { '⁒', "%" },
+        { '⁓', "~" },
+        { '⁕', "*" },
+        { '–', "-" },
+        { '—', "-" },
+        { '​', " " },
+        //{ new Rune((char)55357, (char)56898), ":)" }, // :)
+        //{ new Rune((char)55357, (char)56870), ":(" }, // :)
+    };
+
     public virtual void GenerateDMX(ref List<byte> dmxData)
     {
+        if (!unicode)
+        {
+            //try to downconvert first for common characters
+            string cleaned = "";
+            foreach (char c in text)
+            {
+                if (CleanupTable.TryGetValue(c, out string charValue))
+                {
+                    cleaned += charValue;
+                }
+                else
+                {
+                    cleaned += c;
+                }
+            }
+            text = cleaned;
+        }
+        
         //trim to max characters if the control is present
         if (limitLength && text.Length > maxCharacters)
         {
