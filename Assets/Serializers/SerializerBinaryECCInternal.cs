af5a742bc84fb482887d60ca05d387fed8fb78c9
Matthew Herber <matthewherber@sbcglobal.net>
2025-07-13 19:29:44 -0400

    Possible ECC variant

---
+++ Assets/Serializers/SerializerBinaryECCInternal.cs
@@ -0,0 +1,75 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BinaryECCInternal : IDMXSerializer
+{
+    const int blockSize = 4; // 10x10 pixels per channel block
+    const int blocksPerCol = 270; // channels per column
+
+    public void Construct() { }
+    public void InitFrame() { }
+
+    public void SerializeChannel(ref Color32[] pixels, byte channelValue, int channel, int textureWidth, int textureHeight)
+    {
+        //split the value into 8 bits
+        var bits = new BitArray(new byte[] { channelValue, channelValue, channelValue });
+
+        for (int i = 0; i < bits.Length; i++)
+        {
+            GetPositionData(channel, i, out int x, out int y);
+            if (x >= textureWidth || y >= textureHeight)
+            {
+                continue; // Skip if the calculated pixel is out of bounds
+            }
+            //convert the x y to pixel index
+            //return 4x4 area
+            var color = new Color32(
+                (byte)(bits[i] ? 255 : 0),
+                (byte)(bits[i] ? 255 : 0),
+                (byte)(bits[i] ? 255 : 0),
+                Util.GetBlockAlpha(channelValue)
+            );
+            TextureWriter.MakeColorBlock(ref pixels, x, y, color, blockSize);
+        }
+    }
+
+    public void DeserializeChannel(Texture2D tex, ref byte channelValue, int channel, int textureWidth, int textureHeight)
+    {
+        var bits = new BitArray(8 * 3);
+        for (int i = 0; i < bits.Length; i++)
+        {
+            GetPositionData(channel, i, out int x, out int y);
+            //add on a offset
+            x += 1;
+            y += 1;
+            if (x >= textureWidth || y >= textureHeight)
+            {
+                continue; // Skip if the calculated pixel is out of bounds
+            }
+            // Read the 4x4 area and combine it into a single byte
+            bits[i] = TextureReader.GetColor(tex, x, y).r > 0.5f;
+        }
+        // Convert the BitArray back to a byte
+        channelValue = ConvertToByte(bits);
+    }
+
+    private static void GetPositionData(int channel, int i, out int x, out int y)
+    {
+        int newChannel = (channel * 8 * 3) + i;
+        x = (newChannel / blocksPerCol) * blockSize;
+        y = (newChannel % blocksPerCol) * blockSize;
+    }
+
+    byte ConvertToByte(BitArray bits)
+    {
+        if (bits.Count != 8)
+        {
+            throw new ArgumentException("bits");
+        }
+        byte[] bytes = new byte[1];
+        bits.CopyTo(bytes, 0);
+        return bytes[0];
+    }
+}
