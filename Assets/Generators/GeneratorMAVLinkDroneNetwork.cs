b0e2a11616fd223452dcb3fe856ffa1544132497
Matthew Herber <matthewherber@sbcglobal.net>
2025-07-20 15:48:19 -0400

    Initial working mavlink protocol
    No position mirroring yet, but drone registering works fully

---
+++ Assets/Generators/GeneratorMAVLinkDroneNetwork.cs
@@ -0,0 +1,347 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+using static MAVLink;
+
+public class MAVLinkDroneNetwork : IDMXGenerator
+{
+    private Dictionary<byte, Drone> drones = new Dictionary<byte, Drone>();
+    private UdpClient client;
+    private IPEndPoint selfEndPoint = new IPEndPoint(IPAddress.Any, 0);
+    private IPEndPoint remoteEndPoint = new IPEndPoint(IPAddress.Loopback, 14550);
+    private MavlinkParse parse;
+
+    public int droneCount = 254;
+    public int networkPort = 14550;
+    public void Construct()
+    {
+        client = new UdpClient();
+        remoteEndPoint.Port = networkPort;
+        client.Client.Bind(selfEndPoint);
+        parse = new MavlinkParse();
+
+        Debug.Log($"MAVLink Drone Network started on port {networkPort} with {droneCount} drones.");
+
+        //initialize a certain ammount of drones
+        //drones.Add(5, new Drone(5, client, remoteEndPoint));
+        for (byte i = 1; i <= droneCount; i++)
+        {
+            //Debug.Log(i);
+            drones.Add(i, new Drone(i, client, remoteEndPoint));
+        }
+
+        //startup tasks
+        UniTask.Void(SendData);
+        UniTask.Void(SendHeartbeat);
+        UniTask.Void(ReceiveData);
+    }
+
+    public void GenerateDMX(ref List<byte> dmxData)
+    {
+        return;
+    }
+
+
+    public async UniTaskVoid SendData()
+    {
+        while (Application.isPlaying)
+        {
+            //send data for all drones
+            const int perUpdate = 10; //how many drones to process per update
+            int currentCount = 0;
+            foreach (Drone d in drones.Values)
+            {
+                d.SendGPSRaw();
+                d.SendGPSFiltered();
+                d.SendSYSStatus();
+                currentCount++;
+                if (currentCount == perUpdate)
+                {
+                    //wait for a bit before sending the next batch
+                    await UniTask.Delay(20);
+                    currentCount = 0;
+                }
+            }
+            if (perUpdate > drones.Count)
+            {
+                //wait a bit before sending the next batch because we wont hit the call in the for loop
+                await UniTask.Delay(100);
+            }
+        }
+    }
+
+    //update coroutine
+    public async UniTaskVoid SendHeartbeat()
+    {
+        while (Application.isPlaying)
+        {
+            //process X ammount every time
+            const int perUpdate = 10; //how many drones to process per update
+            int currentCount = 0;
+            foreach (Drone d in drones.Values)
+            {
+                //send heartbeat
+                d.SendHeartbeat();
+                currentCount++;
+                if (currentCount == perUpdate)
+                {
+                    //wait for a bit before sending the next batch
+                    await UniTask.Delay(100);
+                    currentCount = 0;
+                }
+            }
+            if (perUpdate > drones.Count)
+            {
+                //wait a bit before sending the next batch because we wont hit the call in the for loop
+                await UniTask.Delay(100);
+            }
+        }
+    }
+    Stream stream = new MemoryStream();
+    public async UniTaskVoid ReceiveData()
+    {
+        while (Application.isPlaying)
+        {
+            //listen for messages on 14555
+            if (client.Available > 0)
+            {
+                byte[] buffer = client.Receive(ref selfEndPoint);
+                stream.Seek(0, SeekOrigin.Begin);
+                stream.Write(buffer, 0, buffer.Length);
+                stream.Seek(0, SeekOrigin.Begin);
+
+                var message = parse.ReadPacket(stream);
+
+                MAVLINK_MSG_ID messageId = (MAVLINK_MSG_ID)message.msgid;
+
+                //Debug.Log(messageId);
+
+                switch (messageId)
+                {
+                    case MAVLINK_MSG_ID.COMMAND_LONG:
+                        //figure out the command
+                        var obj = (mavlink_command_long_t)message.data;
+                        //find the drone based on the target sysid
+                        bool found = drones.TryGetValue(obj.target_system, out Drone d);
+
+                        if (!found)
+                        {
+                            Debug.LogError($"Invalid system target: {obj.target_system}");
+                            continue;
+                        }
+
+                        //Debug.Log((MAV_CMD)obj.command);
+                        bool handled = false;
+                        switch ((MAV_CMD)obj.command)
+                        {
+                            case MAV_CMD.REQUEST_AUTOPILOT_CAPABILITIES:
+                                handled = true;
+                                d.SendAutopilotCapabilities();
+                                break;
+                            case MAV_CMD.SET_MESSAGE_INTERVAL:
+                                //we dont actually CARE about message intervals, so just ACK it
+                                handled = true;
+                                break;
+                            default:
+                                //handle other commands
+                                Debug.Log($"Unhandled command: {(MAV_CMD)obj.command}");
+                                handled = false;
+                                break;
+                        }
+                        if (handled)
+                        {
+                            d.SendCommandACK((MAV_CMD)obj.command, MAV_RESULT.ACCEPTED, obj.target_system, obj.target_component);
+                        }
+                        break;
+                    default:
+                        Debug.Log($"Unhandled message: {messageId}");
+                        break;
+                }
+            }
+
+            await UniTask.Delay(5);
+        }
+    }
+
+    public class Drone
+    {
+        private MavlinkParse parse;
+        private byte uid; // the valid range for this is 1 to 255
+
+        //lat long and altitude are fixed point numbers
+        private int lat = 1;
+        private int lon = 1;
+        private int alt = 1;
+
+        UdpClient client;
+        IPEndPoint remoteEndPoint;
+
+        private void SetPosition(float lat, float lon, float alt)
+        {
+            const int scalar = 10000000;
+            this.lat = (int)(lat * scalar);
+            this.lon = (int)(lon * scalar);
+            this.alt = (int)(alt * scalar);
+        }
+
+        public Drone(byte uid, UdpClient client, IPEndPoint remoteEndPoint)
+        {
+            this.uid = uid; //0 is not valid
+            this.client = client;
+            this.remoteEndPoint = remoteEndPoint;
+            //SetPosition(globalID * 0.0001f, globalID * 0.0001f, globalID * 0.0001f); // set initial position
+
+            parse = new MavlinkParse();
+        }
+
+        public void SendHeartbeat()
+        {
+            var message = new mavlink_heartbeat_t(
+                127,
+                (byte)MAV_TYPE.GENERIC,
+                (byte)MAV_AUTOPILOT.GENERIC,
+                (byte)MAV_MODE_FLAG.AUTO_ENABLED,
+                (byte)MAV_STATE.STANDBY,
+                MAVLINK_VERSION);
+
+            // Sends a message to the host to which you have connected.
+            Transmit(MAVLINK_MSG_ID.HEARTBEAT, message);
+        }
+
+        public void Transmit<T>(MAVLINK_MSG_ID id, T message)
+            where T : struct
+        {
+            var sendBytes = parse.GenerateMAVLinkPacket20(id, message, sysid: uid);
+
+            client.Send(sendBytes, sendBytes.Length, remoteEndPoint.Address.ToString(), remoteEndPoint.Port);
+        }
+
+        public void SendAutopilotCapabilities()
+        {
+            var message = new mavlink_autopilot_version_t(
+                (ulong)MAV_PROTOCOL_CAPABILITY.SET_POSITION_TARGET_GLOBAL_INT,
+                uid, //random capabilities
+                (uint)FIRMWARE_VERSION_TYPE.BETA,
+                (uint)FIRMWARE_VERSION_TYPE.BETA,
+                (uint)FIRMWARE_VERSION_TYPE.BETA,
+                0,
+                0,
+                0,
+                new byte[8],
+                new byte[8],
+                new byte[8],
+                new byte[18] { (byte)uid, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }
+            );
+
+            Transmit(MAVLINK_MSG_ID.AUTOPILOT_VERSION, message);
+        }
+
+        public void SendCommandACK(MAV_CMD command, MAV_RESULT result, byte target_system, byte target_component, byte progress = byte.MaxValue, int result_param2 = 0)
+        {
+            var message = new mavlink_command_ack_t(
+                (ushort)command,
+                (byte)result,
+                progress,
+                result_param2,
+                target_system,
+                target_component
+            );
+
+            Transmit(MAVLINK_MSG_ID.COMMAND_ACK, message);
+        }
+
+        public void SendLocalPosition()
+        {
+            var message = new mavlink_local_position_ned_t(
+                (uint)DateTimeOffset.UtcNow.ToUnixTimeSeconds(),
+                0,
+                0,
+                0,
+                0,
+                0,
+                0
+            );
+
+            Transmit(MAVLINK_MSG_ID.LOCAL_POSITION_NED, message);
+        }
+
+        public void SendGPSRaw()
+        {
+            var message = new mavlink_gps_raw_int_t(
+                //send current unix time in seconds
+                (ulong)DateTimeOffset.UtcNow.ToUnixTimeSeconds(),
+                lat,
+                lon,
+                alt,
+                ushort.MaxValue,
+                ushort.MaxValue,
+                ushort.MaxValue,
+                ushort.MaxValue,
+                (byte)GPS_FIX_TYPE._3D_FIX,
+                4, //satallites
+                0,
+                0,
+                0,
+                0,
+                0,
+                0
+            );
+
+            Transmit(MAVLINK_MSG_ID.GPS_RAW_INT, message);
+        }
+
+        public void SendGPSFiltered()
+        {
+            var message = new mavlink_global_position_int_t(
+                (uint)DateTimeOffset.UtcNow.ToUnixTimeSeconds(),
+                lat,
+                lon,
+                alt,
+                0,
+                0,
+                0,
+                0,
+                0
+            );
+
+            Transmit(MAVLINK_MSG_ID.GLOBAL_POSITION_INT, message);
+        }
+
+        public void SendSYSStatus()
+        {
+            var flags = MAV_SYS_STATUS_SENSOR.GPS |
+                        MAV_SYS_STATUS_SENSOR.MOTOR_OUTPUTS |
+                        MAV_SYS_STATUS_SENSOR.RC_RECEIVER |
+                        MAV_SYS_STATUS_SENSOR.BATTERY |
+                        MAV_SYS_STATUS_SENSOR.SATCOM |
+                        MAV_SYS_STATUS_SENSOR.PROPULSION;
+            var message = new mavlink_sys_status_t(
+                (uint)flags,
+                (uint)flags,
+                (uint)flags,
+                50,
+                12 * 1000,
+                -1,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                -1
+            );
+
+            Transmit(MAVLINK_MSG_ID.SYS_STATUS, message);
+        }
+    }
+}
