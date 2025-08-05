8a39cf9f8b4139cd238506c502dcfe18396da5d4
Matthew Herber <Matthewherber@sbcglobal.net>
2025-08-05 14:35:34 -0400

    remap UI impl

--- Assets/Plugin/Generators/GeneratorRemap.cs
+++ Assets/Plugin/Generators/GeneratorRemap.cs
@@ -6,9 +6,11 @@ using UnityEngine;
 
 public class Remap : IDMXGenerator
 {
-    public DMXChannel SourceChannelStart { get; set; }
-    public EquationNumber SourceChannelLength { get; set; }
-    public DMXChannel TargetChannel { get; set; }
+    public DMXChannel SourceChannelStart = 0;
+    public EquationNumber SourceChannelLength = 0;
+    public DMXChannel TargetChannel = 0;
+    public void Construct() { }
+    public void Deconstruct() { }
     public void GenerateDMX(ref List<byte> dmxData)
     {
         //ensure capacity
@@ -21,12 +23,34 @@ public class Remap : IDMXGenerator
         }
     }
 
-    public void ConstructUserInterface(RectTransform rect) { }
-    public void DeconstructUserInterface() { }
+    private protected TMP_InputField sourceChannelStartInputfield;
+    private protected TMP_InputField sourceChannelLengthInputfield;
+    private protected TMP_InputField targetChannelInputfield;
+    public void ConstructUserInterface(RectTransform rect)
+    {
+        sourceChannelStartInputfield = Util.AddInputField(rect, "Source Channel Start");
+        sourceChannelStartInputfield.text = SourceChannelStart.ToString();
+        sourceChannelStartInputfield.onEndEdit.AddListener((value) =>
+        {
+            SourceChannelStart = value;
+        });
 
+        sourceChannelLengthInputfield = Util.AddInputField(rect, "Source Channel Length");
+        sourceChannelLengthInputfield.text = SourceChannelLength.ToString();
+        sourceChannelLengthInputfield.onEndEdit.AddListener((value) =>
+        {
+            SourceChannelLength = value;
+        });
+
+        targetChannelInputfield = Util.AddInputField(rect, "Target Channel");
+        targetChannelInputfield.text = TargetChannel.ToString();
+        targetChannelInputfield.onEndEdit.AddListener((value) =>
+        {
+            TargetChannel = value;
+        });
+    }
+
+    public void DeconstructUserInterface() { }
     public void UpdateUserInterface() { }
 
-    public void Construct() { }
-
-    public void Deconstruct() { }
 }
