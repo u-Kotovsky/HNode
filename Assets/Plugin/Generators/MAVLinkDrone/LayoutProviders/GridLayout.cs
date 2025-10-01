cde5f31222a251c99cd8ee840e0bf3f1b3060a86
Matthew Herber <matthewherber@sbcglobal.net>
2025-10-01 15:23:47 -0400

    add rotation option to grid config

--- Assets/Plugin/Generators/MAVLinkDrone/LayoutProviders/GridLayout.cs
+++ Assets/Plugin/Generators/MAVLinkDrone/LayoutProviders/GridLayout.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Collections.Generic;
+using UnityEngine;
 using YamlDotNet.Serialization;
 
 namespace Generators.MAVLinkDrone
@@ -17,6 +19,7 @@ namespace Generators.MAVLinkDrone
         public float gridSpacingLat = 0.0001f; //spacing in degrees
         [YamlMember(Description = "The initial altitude of the drones in meters")]
         public float initialAltitude = 0f;
+        public float rotation = 0f;
         public void LayoutDrones(ref Dictionary<byte, Drone> drones)
         {
             //layout all the drones
@@ -27,9 +30,13 @@ namespace Generators.MAVLinkDrone
                 {
                     //get the drone at the index
                     Drone d = drones[(byte)(dronesLeft)];
+                    Vector2 pos = new Vector2();
+                    pos.x = gridLon + (j * gridSpacingLat);
+                    pos.y = gridLat + ((dronesLeft - 1) / gridLonCount) * gridSpacingLon;
+                    pos = RotateAbout(pos, new Vector2(gridLon, gridLat), rotation);
                     //set the position based on the grid
-                    d.SetPosition(gridLon + (j * gridSpacingLat),
-                                  gridLat + ((dronesLeft - 1) / gridLonCount) * gridSpacingLon,
+                    d.SetPosition(pos.x,
+                                  pos.y,
                                   initialAltitude);
                     dronesLeft--;
                     if (dronesLeft <= 0)
@@ -38,6 +45,19 @@ namespace Generators.MAVLinkDrone
                     }
                 }
             }
+        }
+
+        private Vector2 RotateAbout(Vector2 pos, Vector2 center, float rotation)
+        {
+            //rotate the position around the center by rotation degrees
+            float radians = Mathf.Deg2Rad * rotation;
+            float cos = (float)Math.Cos(radians);
+            float sin = (float)Math.Sin(radians);
+            Vector2 translated = pos - center;
+            Vector2 rotated = new Vector2();
+            rotated.x = translated.x * cos - translated.y * sin;
+            rotated.y = translated.x * sin + translated.y * cos;
+            return rotated + center;
         }
     }
 }
