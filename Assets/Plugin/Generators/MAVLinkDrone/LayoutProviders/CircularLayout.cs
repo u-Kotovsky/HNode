2cefaed71bfe8247dc335ced4ec6a003125e196e
Matthew Herber <matthewherber@sbcglobal.net>
2025-10-06 15:01:18 -0400

    add rotation offset system

--- Assets/Plugin/Generators/MAVLinkDrone/LayoutProviders/CircularLayout.cs
+++ Assets/Plugin/Generators/MAVLinkDrone/LayoutProviders/CircularLayout.cs
@@ -14,15 +14,19 @@ namespace Generators.MAVLinkDrone
         public float Radius = 0.0001f; //spacing in degrees
         [YamlMember(Description = "The initial altitude of the drones in meters")]
         public float initialAltitude = 0f;
+        [YamlMember(Description = "0 to 1 float representing the rotation offset from the default, and the next drone position")]
+        public float rotationOffset = 0f;
         public void LayoutDrones(ref Dictionary<byte, Drone> drones)
         {
+            float angleStep = 360f / drones.Count;
             //layout all the drones
             for (int j = 0; j < drones.Count; j++)
             {
                 //get the drone at the index
                 Drone d = drones[(byte)(j + 1)];
                 //set the position based on the grid
-                float angle = 360f / (drones.Count) * j;
+                float angle = angleStep * j;
+                angle += rotationOffset * angleStep;
                 //get a direction vector2
                 Vector2 dir = new Vector2(Mathf.Cos(angle * Mathf.Deg2Rad), Mathf.Sin(angle * Mathf.Deg2Rad));
                 Vector2 pos = dir * Radius;
